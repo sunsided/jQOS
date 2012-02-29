04e5f2b1ec1b96e99c2a9e30ea7e01a62fa60f0a
Markus Mayer <development@widemeadows.de>
2012-02-29 13:26:05 +0100

    Schritt in Richtung Objektorientierung

--- js/jqos.js
+++ js/jqos.js
@@ -1,5 +1,124 @@
 $(function() {
 
+    function JQOSConsole() {
+
+        function removeCurrentCaret(caret) {
+            caret = caret || $('.console-caret');
+            if (caret.hasClass('console-char')) {
+                caret.removeClass('console-caret hi lo');
+                caret.stopBlink();
+            }
+            else {
+                caret.remove();
+            }
+        }
+
+        this.keyPressed = function(code) {
+            // Zeile zusammensetzen: Einzelnes Zeichen einfügen
+            var character = String.fromCharCode(code);
+            $('<div class="console-char volatile">'+character+'</div>').insertBefore('.console-caret');
+
+        };
+
+        this.returnPressed = function() {
+            // TODO: Was passiert, wenn Newline nicht am Zeilenende ausgeführt wird?
+
+            // Volatile-Markierung entfernen
+            $('.console-char').removeClass('volatile');
+
+            // Caret entfernen und neue Zeile einfügen
+            removeCurrentCaret();
+            $('<div class="console-feedback-line"><div class="console-caret hi"></div></div>').insertAfter($('.console-feedback-line').last());
+            $('.console-caret').blink();
+        };
+
+        this.deletePressed = function(direction) {
+            var element;
+            if (direction == 0) {
+                element = $('.console-caret').prev();
+                if (element.length > 0) element.remove();
+            }
+            else {
+                element = $('.console-caret');
+                if (element.hasClass('console-char')) {
+                    // Caret nach rechts, dann aktuelles Zeichen löschen
+                    $(document).trigger('console-cursor-right');
+                    element.remove();
+                }
+            }
+        };
+
+        this.cursorLeft = function () {
+            // Ermitteln, ob der Caret ein echtes Zeichen ist
+            var caret = $('.console-caret');
+
+            // Zeichen vor dem Caret ermitteln
+            var prev = caret.prev();
+            if (prev.length == 0) return;
+
+            // Caret nach links verschieben
+            prev.addClass('console-caret hi');
+            prev.blink();
+
+            // Caret ist auch ein character
+            removeCurrentCaret(caret);
+        };
+
+        this.cursorRight = function() {
+            // Caret beziehen
+            var caret = $('.console-caret');
+
+            // Prüfen, ob aktueller Caret ein echter Caret ist
+            if (!caret.hasClass('console-char')) return;
+
+            // Zeichen nach dem Caret ermitteln
+            var next = caret.next();
+            if (next.length == 0) {
+                // Echten Caret erzeugen
+                next = $('<div></div>');
+                next.insertAfter(caret);
+            }
+
+            // Caret-Markierung hinzufügen
+            next.addClass('console-caret hi');
+            next.blink();
+
+            // Alte Caret-Markierung entfernen
+            caret.removeClass('console-caret hi lo');
+            caret.stopBlink();
+        };
+
+        this.jumpPressed = function(direction) {
+            var newTarget;
+            if (direction == 0) {
+                newTarget = $('.console-char.volatile').first();
+                if (newTarget.length > 0) {
+
+                    // Aktuellen Caret abwählen
+                    removeCurrentCaret();
+
+                    // Neues Ziel setzen
+                    newTarget.addClass('console-caret hi');
+                    newTarget.blink();
+                }
+            }
+            else {
+                newTarget = $('.console-char.volatile').last();
+                if (newTarget.length > 0) {
+                    // Aktuellen Caret abwählen
+                    removeCurrentCaret();
+
+                    next = $('<div></div>');
+                    next.insertAfter(newTarget);
+                    next.addClass('console-caret hi');
+                    next.blink();
+                }
+            }
+        }
+    }
+
+    var console = new JQOSConsole();
+
     // Caret blinken lassen
     $('.console-caret').blink();
 
@@ -70,149 +189,59 @@ $(function() {
 
     // generic key pressed
     $(document).bind('console-key-pressed', function(event, code) {
-        var character = String.fromCharCode(code);
-
-        // Zeile zusammensetzen: Einzelnes Zeichen einfügen
-        $('<div class="console-char volatile">'+character+'</div>').insertBefore('.console-caret');
+        console.keyPressed(code);
     });
 
     // return pressed
-    $(document).bind('console-return-pressed', function(event) {
-
-        // TODO: Was passiert, wenn Newline nicht am Zeilenende ausgeführt wird?
-
-        // Volatile-Markierung entfernen
-        $('.console-char').removeClass('volatile');
-
-        // Caret entfernen und neue Zeile einfügen
-        removeCurrentCaret();
-        $('<div class="console-feedback-line"><div class="console-caret hi"></div></div>').insertAfter($('.console-feedback-line').last());
-        $('.console-caret').blink();
+    $(document).bind('console-return-pressed', function () {
+        console.returnPressed();
     });
 
     // delete pressed
     $(document).bind('console-delete-pressed', function(event, direction) {
-        if (direction == 0) {
-            var element = $('.console-caret').prev();
-            if (element.length > 0) element.remove();
-        }
-        else {
-            var element = $('.console-caret');
-            if (element.hasClass('console-char')) {
-                // Caret nach rechts, dann aktuelles Zeichen löschen
-                $(document).trigger('console-cursor-right');
-                element.remove();
-            }
-        }
+        console.deletePressed(direction);
     });
 
     // delete pressed
-    $(document).bind('console-insert-pressed', function(event) {
+    $(document).bind('console-insert-pressed', function () {
         // alert("insert!");
     });
 
     // delete pressed
-    $(document).bind('console-page-pressed', function(event, direction) {
+    $(document).bind('console-page-pressed', function (/*direction*/) {
         // alert(direction == 0 ? "page up!" : "page down!");
     });
 
-    function removeCurrentCaret(caret) {
-        caret = caret || $('.console-caret');
-        if (caret.hasClass('console-char')) {
-            caret.removeClass('console-caret hi lo');
-            caret.stopBlink();
-        }
-        else {
-            caret.remove();
-        }
-    }
-
     // delete pressed
-    $(document).bind('console-jump-pressed', function(event, direction) {
-        if (direction == 0) {
-            var newTarget = $('.console-char.volatile').first();
-            if (newTarget.length > 0) {
-
-                // Aktuellen Caret abwählen
-                removeCurrentCaret();
-
-                // Neues Ziel setzen
-                newTarget.addClass('console-caret hi');
-                newTarget.blink();
-            }
-        }
-        else {
-            var newTarget = $('.console-char.volatile').last();
-            if (newTarget.length > 0) {
-                // Aktuellen Caret abwählen
-                removeCurrentCaret();
-
-                next = $('<div></div>');
-                next.insertAfter(newTarget);
-                next.addClass('console-caret hi');
-                next.blink();
-            }
-        }
+    $(document).bind('console-jump-pressed', function(direction) {
+        console.jumpPressed(direction);
     });
 
     // cursor left pressed
-    $(document).bind('console-cursor-left', function(event) {
-        // Ermitteln, ob der Caret ein echtes Zeichen ist
-        var caret = $('.console-caret');
-
-        // Zeichen vor dem Caret ermitteln
-        var prev = caret.prev();
-        if (prev.length == 0) return;
-
-        // Caret nach links verschieben
-        prev.addClass('console-caret hi');
-        prev.blink();
-
-        // Caret ist auch ein character
-        removeCurrentCaret(caret);
+    $(document).bind('console-cursor-left', function () {
+        console.cursorLeft();
     });
 
     // cursor right pressed
-    $(document).bind('console-cursor-right', function(event) {
-        // Caret beziehen
-        var caret = $('.console-caret');
-
-        // Prüfen, ob aktueller Caret ein echter Caret ist
-        if (!caret.hasClass('console-char')) return;
-
-        // Zeichen nach dem Caret ermitteln
-        var next = caret.next();
-        if (next.length == 0) {
-            // Echten Caret erzeugen
-            next = $('<div></div>');
-            next.insertAfter(caret);
-        }
-
-        // Caret-Markierung hinzufügen
-        next.addClass('console-caret hi');
-        next.blink();
-
-        // Alte Caret-Markierung entfernen
-        caret.removeClass('console-caret hi lo');
-        caret.stopBlink();
+    $(document).bind('console-cursor-right', function () {
+        console.cursorRight();
     });
 
     // cursor up pressed
-    $(document).bind('console-cursor-up', function(event) {
+    $(document).bind('console-cursor-up', function () {
         // alert("go up!");
     });
 
     // cursor down pressed
-    $(document).bind('console-cursor-down', function(event) {
+    $(document).bind('console-cursor-down', function () {
         // alert("go down!");
     });
 
 });
 
-jQuery.fn.blink = function(options)
-{
+$.fn.blink = function(options) {
     var defaults = { delay:500 };
-    var options = $.extend(defaults, options);
+    var opts = $.extend(defaults, options);
 
     return this.each(function()
     {
@@ -227,13 +256,12 @@ jQuery.fn.blink = function(options)
             {
                 $(obj).removeClass('lo').addClass('hi');
             }
-        }, options.delay);
+        }, opts.delay);
         obj.data('blink-interval-id', intervalID);
     });
 };
 
-jQuery.fn.stopBlink = function()
-{
+$.fn.stopBlink = function() {
     return this.each(function()
     {
         var $obj = $(this);
