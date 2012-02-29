bb93bb34cef6c6f05c9ecd988eb9b8247243b44e
Markus Mayer <development@widemeadows.de>
2012-02-29 04:00:40 +0100

    Caret-Logik verändert und Backspace ermöglicht

--- js/jqos.js
+++ js/jqos.js
@@ -1,7 +1,7 @@
 $(function() {
 
     // Caret blinken lassen
-    $('#console-input-caret').blink();
+    $('.console-caret').blink();
 
     // Keycode grabben
     $(document).keydown(function(event) {
@@ -70,25 +70,47 @@ $(function() {
 
     // generic key pressed
     $(document).bind('console-key-pressed', function(event, code) {
-        $('#console-input').val($('#console-input').val() + String.fromCharCode(code));
+        var character = String.fromCharCode(code);
+
+        // Ausgabe zu Debugzwecken
+        $('#console-input').val($('#console-input').val() + character);
+
+        // Zeile zusammensetzen: Einzelnes Zeichen einfügen
+        $('<div class="console-char volatile">'+character+'</div>').insertBefore('.console-caret');
     });
 
     // return pressed
     $(document).bind('console-return-pressed', function(event) {
 
-        // Text beziehen
-        var line = $('#console-input').val();
+        // Volatile-Markierung entfernen
+        $('.console-char').removeClass('volatile');
 
+        // Text beziehen
+        var line = $('#console-input').val(); // TODO: Auswerten!
+
+        /*
         // Text einfügen
-        $('<div>'+line+'</div>').insertBefore('#console-input-feedback');
+        $('<div class="console-feedback-line">'+line+'</div>').insertBefore('#console-caret');
+        */
 
         // Eingabe leeren
         $('#console-input').val('');
+
+        // Caret entfernen und neue Zeile einfügen
+        $('.console-caret').remove();
+        $('<div class="console-feedback-line"><div class="console-caret"></div></div>').insertAfter($('.console-feedback-line').last());
+        $('.console-caret').blink();
     });
 
     // delete pressed
     $(document).bind('console-delete-pressed', function(event, direction) {
-        alert(direction == 0 ? "backspace!" : "delete!");
+        if (direction == 0) {
+            var element = $('.console-caret').prev();
+            if (element != null) element.remove();
+        }
+        else {
+            alert("delete!");
+        }
     });
 
     // delete pressed
@@ -138,13 +160,13 @@ jQuery.fn.blink = function(options)
         var obj = $(this);
         setInterval(function()
         {
-            if($(obj).css("visibility") == "visible")
+            if($(obj).hasClass("hi"))
             {
-                $(obj).css('visibility','hidden');
+                $(obj).removeClass('hi').addClass('lo');
             }
             else
             {
-                $(obj).css('visibility','visible');
+                $(obj).removeClass('lo').addClass('hi');
             }
         }, options.delay);
     });
