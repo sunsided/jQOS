ff83813414fafd3374d359fed46410e51e6183f9
Markus Mayer <development@widemeadows.de>
2012-02-29 18:59:24 +0100

    Nonsens auf Serverseite

--- server.php
+++ server.php
@@ -2,7 +2,15 @@
 	
 	$command = $_REQUEST["command"];
 
-	// sleep(1);
-	$result = array ('message'=> 'SYNTAX ERROR: '.$command);
+	// Sonderkommandos
+	if ($command == "date") {
+		die(json_encode(array('message' => date(DATE_RFC822))));
+	}
+	elseif ($command == "meow") {
+		die(json_encode(array('message' => ':3')));
+	}
+	
+	// generischer Fall
+	$result = array ('message'=> 'SYNTAX ERROR');
 	die(json_encode($result));
 	
